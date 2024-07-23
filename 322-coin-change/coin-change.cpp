63775cb84193310bbc9623cbaa1594033ca0eba2
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-07-24 04:19:52 +0530

    Time: 50 ms (26.97%) | Memory: 47 MB (35.86%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -15,12 +15,29 @@ public:
         return dp[ind][amt] = min(notTake , take);
 
     }
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int amt) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int>(amount+1 , -1));
+        vector<vector<int>> dp(n, vector<int>(amt+1 , -1));
 
-        int ans = f(n-1, amount, coins, dp);
+        for(int i=0; i<=amt; i++){
+            if(i % coins[0] == 0) dp[0][i] = i/coins[0];
+            else dp[0][i] = 1e9;
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<= amt; j++){
+                int notTake = 0 + dp[i-1][j];
+                int take = INT_MAX;
+                if(coins[i] <= j){
+                    take = 1 + dp[i][j-coins[i]];
+                }
+
+                dp[i][j] = min(notTake , take);
+            }
+        }
+
+        int ans = dp[n-1][amt];
 
         if(ans >= 1e9) return -1;
         else return ans;
