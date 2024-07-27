21b2cd47f147e4d6a3062a46af369455ecfffe05
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-07-27 13:30:04 +0530

    Time: 7 ms (71.52%) | Memory: 14.3 MB (38.71%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+	 int f(int ind, int target, vector<int>& nums, vector<vector<int>> &dp){
+        // if(target == 0) return 1;
+        if(ind==0){
+            if(target==0 && nums[0]==0) return 2;
+            if(nums[0]==target || target==0) return 1;
+            return 0;
+        }
+        if(dp[ind][target] != -1) return dp[ind][target];
+        int notTake = f(ind-1, target, nums, dp);
+        int take = 0;
+        if(nums[ind] <= target){
+            take = f(ind-1, target-nums[ind], nums, dp);
+        }
+
+        return dp[ind][target] = (take + notTake );
+    }
+    
+
+    int findTargetSumWays(vector<int>& nums, int d) {
+        int n = nums.size();
+
+        int sum=0;
+        
+        for(int i=0; i<n;i++){
+            sum+=nums[i];
+        }
+        
+        if(sum + d < 0 ||(sum+d) % 2 == 1) return 0;
+        
+        int tar = (sum+d)/2;
+        
+        vector<vector<int>> dp(n, vector<int>(tar+1 , -1));
+        return f(n-1, tar, nums, dp);
+    }
+};
