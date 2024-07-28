da5c9bd6eed96f567bfaad784b9437099580583a
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-07-28 14:42:25 +0530

    Time: 3 ms (92.43%) | Memory: 20.2 MB (28.44%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int f(int ind, int amt,  vector<int>& coins, vector<vector<int>>& dp){
+        if(ind == 0){
+            if(amt%coins[ind]==0) return 1;
+            else return 0;
+        }
+        if(amt == 0){
+            return 1;
+        }
+        if(dp[ind][amt]!=-1)return dp[ind][amt];
+        int notTake = f(ind-1, amt, coins, dp);
+        int take = 0;
+        if(amt>=coins[ind]){
+            take = f(ind, amt-coins[ind], coins, dp);
+        }
+
+        return dp[ind][amt] = take + notTake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+
+        return f(n-1, amount, coins, dp);
+    }
+};
