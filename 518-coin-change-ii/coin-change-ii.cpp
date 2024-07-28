44bd754bbd480618a87fe9febf7ccaef7469e5df
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-07-28 15:15:47 +0530

    Time: 16 ms (30.12%) | Memory: 20 MB (39.67%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -5,9 +5,6 @@ public:
             if(amt%coins[ind]==0) return 1;
             else return 0;
         }
-        // if(amt == 0){
-        //     return 1;
-        // }
         if(dp[ind][amt]!=-1)return dp[ind][amt];
         int notTake = f(ind-1, amt, coins, dp);
         int take = 0;
@@ -20,10 +17,26 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-
-        return f(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
 
         
+
+        for(int i = 0 ; i<=amount; i++){
+            if(i%coins[0]==0) dp[0][i] = 1;
+        }
+
+        for(int ind=1; ind<n; ind++){
+            for(int amt = 0; amt<=amount; amt++){
+                int notTake = dp[ind-1][amt];
+                int take = 0;
+                if(amt>=coins[ind]){
+                    take = dp[ind][amt-coins[ind]];
+                }
+
+                dp[ind][amt] = take + notTake;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 };
