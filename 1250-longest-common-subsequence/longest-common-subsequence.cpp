ab8dfc67059466c6625bc7e82043bdd6dfa673cf
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-07-30 03:29:56 +0530

    Time: 15 ms (93.91%) | Memory: 8.1 MB (89.46%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        // Base case is covered as we have initialized the prev and cur vectors to 0.
+
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1[ind1 - 1] == s2[ind2 - 1])
+                    cur[ind2] = 1 + prev[ind2 - 1]; // Characters match, increment LCS length
+                else
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]); // Characters don't match, consider the maximum from above or left
+            }
+            prev = cur; // Update the previous row with the current row
+        }
+
+        return prev[m];
+    }
+};
