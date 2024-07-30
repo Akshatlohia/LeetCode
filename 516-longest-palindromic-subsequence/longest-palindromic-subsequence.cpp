92a1f6164a9822fcfddb293bbaa12154e5b70bba
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-07-31 04:06:58 +0530

    Time: 52 ms (79.13%) | Memory: 8.1 MB (90.08%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    int f(string s1, string s2, int ind1, int ind2, vector<vector<int>> &dp){
-        if(ind1 == 0 || ind2 == 0){
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        if(s1[ind1-1] == s2[ind2-1]){
-            return dp[ind1][ind2] = 1+f(s1, s2, ind1-1, ind2-1, dp);
-        }
-        else{
-            return dp[ind1][ind2] = max(f(s1, s2, ind1-1, ind2, dp),f(s1, s2, ind1, ind2-1, dp));
-        }
-    }
+    // int f(string s1, string s2, int ind1, int ind2, vector<vector<int>> &dp){
+    //     if(ind1 == 0 || ind2 == 0){
+    //         return 0;
+    //     }
+    //     if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+    //     if(s1[ind1-1] == s2[ind2-1]){
+    //         return dp[ind1][ind2] = 1+f(s1, s2, ind1-1, ind2-1, dp);
+    //     }
+    //     else{
+    //         return dp[ind1][ind2] = max(f(s1, s2, ind1-1, ind2, dp),f(s1, s2, ind1, ind2-1, dp));
+    //     }
+    // }
     int longestPalindromeSubseq(string s) {
         string s2 = s;
         int n = s.size();
         reverse(s2.begin(), s2.end());
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        vector<int>curr(n+1, 0);
+        vector<int>prev(n+1, 0);
 
         for(int ind1 = 1; ind1<=n; ind1++){
             for(int ind2 = 1; ind2<=n; ind2++){
                 if(s[ind1-1] == s2[ind2-1]){
-                    dp[ind1][ind2] = 1+dp[ind1-1][ind2-1];
+                    curr[ind2] = 1+prev[ind2-1];
                 }
                 else{
-                    dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
-                } 
+                    curr[ind2] = max(prev[ind2],curr[ind2-1]);
+                }
             }
+            prev = curr; 
         }
 
-        return dp[n][n];
+        return prev[n];
     }
 };
