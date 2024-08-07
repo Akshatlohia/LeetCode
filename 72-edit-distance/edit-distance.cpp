f5678243d830ad188de05cd95b6eb3c6bf76b8c0
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-08-07 19:25:01 +0530

    Time: 9 ms (49.59%) | Memory: 12.8 MB (43.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int f(string word1, string word2, int i, int j, vector<vector<int>>& dp){
 
-        if(j<0){
-            return i+1;
+        if(j==0){
+            return i;
         }
-        if(i<0){
-            return j+1;
+        if(i==0){
+            return j;
         }
 
         if(dp[i][j]!=-1)return dp[i][j];
-        if(word1[i]==word2[j]){
+        if(word1[i-1]==word2[j-1]){
             return dp[i][j] = f(word1, word2, i-1, j-1, dp);
         }
         else{
@@ -30,8 +30,32 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        return f(word1, word2, n-1, m-1, dp);
+        for(int i=1; i<=n; i++){
+            dp[i][0] = i; 
+        }
+
+        for(int j=1; j<=m; j++){
+            dp[0][j] = j; 
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insert = 1 + dp[i][j-1];
+
+                    int del = 1 + dp[i-1][j];
+
+                    int replace = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min(insert, min(del, replace));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
