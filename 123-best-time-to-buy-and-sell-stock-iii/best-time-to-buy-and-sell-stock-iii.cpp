510a2e18cfd5345d325fe86202478dc45866cfe5
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-08-12 17:50:55 +0530

    Time: 213 ms (65.94%) | Memory: 138.1 MB (62.86%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,10 +4,10 @@ public:
         if (ind == n) {
             return 0;
         }
-        if (buy >= 4){
+        if (buy == 4){
             return 0;
         }
-        
+
         if (dp[ind][buy] != -1) {
             return dp[ind][buy];
         }
@@ -27,13 +27,24 @@ public:
 
     int maxProfit(vector<int>& Arr) {
         int n = Arr.size();
-        vector<vector<int>> dp(n, vector<int>(4, -1));
+        
+        vector<vector<int>> dp(n+1, vector<int>(5, 0));
 
-        if (n == 0) {
-            return 0;
+        for(int ind = n-1; ind>=0; ind--){
+            for(int buy = 3; buy>=0; buy--){
+                int profit = 0;
+                if (buy%2 == 0) { 
+                    profit = max(0 + dp[ind + 1][buy], -Arr[ind] + dp[ind + 1][buy+1]);
+                }
+
+                if (buy%2 == 1) { 
+                    profit = max(0 + dp[ind + 1][buy], Arr[ind] + dp[ind + 1][buy+1]);
+                }
+                dp[ind][buy] = profit;
+            }
         }
 
-        int ans = getAns(Arr, 0, 0, n, dp);
+        int ans = dp[0][0];
         return ans;
     }
 };
