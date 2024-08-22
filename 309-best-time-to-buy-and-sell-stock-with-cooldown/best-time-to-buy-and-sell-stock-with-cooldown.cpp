948e5a759eb2e0835d6c4eda2368e817fa8101a7
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-08-22 20:48:10 +0530

    Time: 4 ms (57.23%) | Memory: 14.6 MB (31.87%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -3,17 +3,17 @@ public:
     int maxProfit(vector<int>& Arr) {
         int n = Arr.size();
         
-        vector<vector<int>> dp(n+2, vector<int>(n+1, 0));
+        vector<vector<int>> dp(n+2, vector<int>(2, 0));
 
         for(int ind = n-1; ind>=0; ind--){
-            for(int buy = n-1; buy>=0; buy--){
+            for(int buy = 1; buy>=0; buy--){
                 int profit = 0;
-                if (buy%2 == 0) { 
-                    profit = max(0 + dp[ind + 1][buy], -Arr[ind] + dp[ind + 1][buy+1]);
+                if (buy == 0) { 
+                    profit = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
                 }
 
-                if (buy%2 == 1) { 
-                    profit = max(0 + dp[ind + 1][buy], Arr[ind] + dp[ind + 2][buy+1]);
+                if (buy == 1) { 
+                    profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 2][0]);
                 }
                 dp[ind][buy] = profit;
             }
