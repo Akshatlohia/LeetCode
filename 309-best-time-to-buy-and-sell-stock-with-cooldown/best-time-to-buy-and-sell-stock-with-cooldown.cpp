b50d5684adb8733e67626e87e96c98e276c33789
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-08-22 20:53:38 +0530

    Time: 9 ms (17.52%) | Memory: 14.5 MB (51.09%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,21 +6,13 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(2, 0));
 
         for(int ind = n-1; ind>=0; ind--){
-            for(int buy = 1; buy>=0; buy--){
-                int profit = 0;
-                if (buy == 0) { 
-                    profit = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
-                }
+                    dp[ind][0] = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
 
-                if (buy == 1) { 
                     if(ind == n-1){
-                        profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
+                        dp[ind][1] = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 1][0]);
                     }
                     else
-                        profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 2][0]);
-                }
-                dp[ind][buy] = profit;
-            }
+                        dp[ind][1] = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 2][0]);
         }
 
         int ans = dp[0][0];
