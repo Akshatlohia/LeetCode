1634bcc863e5731115445effbb46ed9ca5adbbff
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-09-04 22:30:34 +0530

    Time: 11 ms (46.60%) | Memory: 16.9 MB (63.48%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -16,27 +16,28 @@ public:
             }
         }
 
-        for(int j=m-1; j>=1; j--){
-            if(matrix[0][j]==0){
-                for(int i=1;i<n;i++){
-                    if(matrix[i][j] != 0) matrix[i][j] = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (matrix[i][j] != 0) {
+                // check for col & row:
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
+    }
 
-        for(int i=n-1; i>=0; i--){
-            if(matrix[i][0]==0){
-                for(int j=1;j<m;j++){
-                    if(matrix[i][j] != 0) matrix[i][j] = 0;
-                }
-            }
+    //step 3: Finally mark the 1st col & then 1st row:
+    if (matrix[0][0] == 0) {
+        for (int j = 0; j < m; j++) {
+            matrix[0][j] = 0;
         }
-
-        if(col0 == 0){
-            for(int i=0;i<n;i++){
-                if(matrix[i][0] != 0) matrix[i][0] = 0;
-            }
+    }
+    if (col0 == 0) {
+        for (int i = 0; i < n; i++) {
+            matrix[i][0] = 0;
         }
+    }
 
     }
 };
