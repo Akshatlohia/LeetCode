09c9ec9b1cd00bc9d4be688b93a52ebf93b3e39d
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-09-05 02:17:44 +0530

    Time: 87 ms (11.96%) | Memory: 70.6 MB (21.59%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,60 +1,64 @@
 class Solution {
 public:
-    int f(int ind, int flag, vector<int>& nums, vector<vector<int>>& dp){
-        if(ind == 0){
-            if(flag == 0){
-                return -1e8;
-            }
-            return 0;
-        }
+    // int f(int ind, int flag, vector<int>& nums, vector<vector<int>>& dp){
+    //     if(ind == 0){
+    //         if(flag == 0){
+    //             return -1e8;
+    //         }
+    //         return 0;
+    //     }
 
 
-        if(dp[ind][flag] != -1) return dp[ind][flag];
+    //     if(dp[ind][flag] != -1) return dp[ind][flag];
 
-        if(flag == 0){
-            int pick = nums[ind-1] + f(ind-1, flag+1, nums, dp);
+    //     if(flag == 0){
+    //         int pick = nums[ind-1] + f(ind-1, flag+1, nums, dp);
 
-            int notPick = f(ind-1, flag, nums, dp);
+    //         int notPick = f(ind-1, flag, nums, dp);
 
-            return dp[ind][flag] = max(pick, notPick);
-        }
+    //         return dp[ind][flag] = max(pick, notPick);
+    //     }
 
-        if(flag == 1){
-            int pick = nums[ind-1] + f(ind-1, flag, nums, dp);
+    //     if(flag == 1){
+    //         int pick = nums[ind-1] + f(ind-1, flag, nums, dp);
 
-            return dp[ind][flag] = max(pick, 0);
-        }
+    //         return dp[ind][flag] = max(pick, 0);
+    //     }
 
-        return 0;
-    }
+    //     return 0;
+    // }
 
 
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        // vector<vector<int>> dp(n+1, vector<int>(2, 0));
 
-        dp[0][0] = -1e8;
+        vector<int> prev(2,0);
+        vector<int> curr(2,0);
+
+        prev[0] = -1e8;
 
         for(int ind=1;ind<=n;ind++){
             for(int flag=0;flag<2;flag++){
                 if(flag == 0){
-                    int pick = nums[ind-1] + dp[ind-1][flag+1];
+                    int pick = nums[ind-1] + prev[flag+1];
 
-                    int notPick = dp[ind-1][flag];
+                    int notPick = prev[flag];
 
-                    dp[ind][flag] = max(pick, notPick);
+                    curr[flag] = max(pick, notPick);
                 }
 
                 if(flag == 1){
-                    int pick = nums[ind-1] + dp[ind-1][flag];
+                    int pick = nums[ind-1] + prev[flag];
 
-                    dp[ind][flag] = max(pick, 0);
+                    curr[flag] = max(pick, 0);
                 }
             }
+            prev = curr;
         }
 
 
 
-        return dp[n][0];
+        return prev[0];
     }
 };
