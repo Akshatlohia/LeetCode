2170445abc44a188adf09ca23a2b7e60534d6c4e
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-09-05 01:42:57 +0530

    Time: 193 ms (5.20%) | Memory: 120.9 MB (5.07%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int f(int ind, int flag, vector<int>& nums, vector<vector<int>>& dp){
+        if(flag == 2){
+            return 0;
+        }
+
+        if(ind<0){
+            if(flag == 0){
+                return -1e8;
+            }
+            return 0;
+        }
+
+
+        if(dp[ind][flag] != -1) return dp[ind][flag];
+
+        if(flag == 0){
+            int pick = nums[ind] + f(ind-1, flag+1, nums, dp);
+
+            int notPick = f(ind-1, flag, nums, dp);
+
+            return dp[ind][flag] = max(pick, notPick);
+        }
+
+        if(flag == 1){
+            int pick = nums[ind] + f(ind-1, flag, nums, dp);
+
+            int notPick = f(ind-1, flag+1, nums, dp);
+
+            return dp[ind][flag] = max(pick, notPick);
+        }
+
+        return 0;
+    }
+
+
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        return f(n-1, 0, nums, dp);
+    }
+};
