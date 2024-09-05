5512ccf4102132f610404e369b74904fb68189eb
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-09-05 19:34:47 +0530

    Time: 25 ms (70.41%) | Memory: 38.4 MB (60.31%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -10,19 +10,13 @@ public:
             pre[i] = pre[i-1]*nums[i-1];
         }
 
-        int s = nums[n-1];
-        nums[n-1] = 1;
-        for(int i = n-2; i>=0; i--){
-            int num = nums[i+1]*s;
-            s=nums[i];
-            nums[i]=num;
-        }
-
-        for(int i=0; i<n; i++){
-            nums[i] = nums[i]*pre[i];
+        int s = 1;
+        for(int i = n-1; i>=0; i--){
+            pre[i] = pre[i]*s;
+            s=s*nums[i];
         }
 
 
-        return nums;
+        return pre;
     }
 };
