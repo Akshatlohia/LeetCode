caa34817554301c1dac4c52b5eef71d1ad0a9b30
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-09-17 20:40:27 +0530

    Time: 43 ms (47.09%) | Memory: 58.2 MB (65.82%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,23 +6,19 @@ public:
         int maxLen = 0;
         
         while (right < nums.size()) {
-            // If the current element is 0, increment the zero count
             if (nums[right] == 0) {
                 zeroCount++;
             }
             
-            // If the number of zeros exceeds k, shrink the window from the left
             if (zeroCount > k) {
                 if (nums[left] == 0) {
                     zeroCount--;
                 }
-                left++; // Move the left pointer to shrink the window
+                left++; 
             }
             
-            // Update the maximum window length
-            maxLen = max(maxLen, right - left + 1);
+            if(zeroCount <= k) maxLen = max(maxLen, right - left + 1);
             
-            // Expand the window by moving the right pointer
             right++;
         }
         
