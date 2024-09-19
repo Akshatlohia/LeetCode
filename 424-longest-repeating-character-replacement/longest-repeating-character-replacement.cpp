f4d62735b818d138900f7877ba3186616f9c5d57
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-09-19 15:51:02 +0530

    Time: 5 ms (80.43%) | Memory: 8.5 MB (54.54%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-int findMax(int arr[], int size) {
-    int maxElement = arr[0]; // Initialize with the first element
-
-    for (int i = 1; i < size; i++) {
-        if (arr[i] > maxElement) {
-            maxElement = arr[i]; // Update maxElement if a larger value is found
-        }
-    }
-
-    return maxElement;
-}
     int characterReplacement(string s, int k) {
         int arr[26] = {0};
         int l=0;
@@ -18,7 +7,7 @@ int findMax(int arr[], int size) {
         int len = 0;
         while(r < s.size()){
             arr[s[r]-'A']++;
-            if((r-l+1) - findMax(arr, 26) <= k){
+            if((r-l+1) - *max_element(arr, arr + 26) <= k){
                 len = max(len, r-l+1);
             }
             else{
