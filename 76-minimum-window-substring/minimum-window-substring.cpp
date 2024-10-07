f956c4d842033eaa8cb72390bb7d02d286f9d911
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-10-07 14:41:01 +0530

    Time: 12 ms (71.87%) | Memory: 10.1 MB (78.81%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        if(m>n) return "";
+        vector<int> mpp(256, 0);
+        for(int i=0; i<m; i++){
+            mpp[t[i]]++;
+        }
+
+        int l=0;
+        int r=0;
+        int cnt = 0;
+        int mini = 1e8;
+        int minii = 0;
+        while(r < n){
+            if(mpp[s[r]] > 0){
+                cnt++;
+            }
+            mpp[s[r]]--;
+
+            while(cnt == m){
+                int size = r-l+1;
+                if(size<mini){
+                    mini = size;
+                    minii = l;
+                }
+                mpp[s[l]]++;
+                if(mpp[s[l]] > 0) cnt--;
+                l++;
+            }
+
+            
+
+            r++;
+        }
+        return mini == 1e8 ? "" : s.substr(minii, mini);
+ 
+    }
+};
