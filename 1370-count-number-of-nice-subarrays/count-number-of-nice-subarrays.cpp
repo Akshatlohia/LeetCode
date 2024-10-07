83c213bd037aecb01bd7ce084e6615ba44f794f6
Akshatlohia <97466631+Akshatlohia@users.noreply.github.com>
2024-10-07 12:14:02 +0530

    Time: 104 ms (27.93%) | Memory: 70.3 MB (37.51%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-    int left = 0, right = 0, oddCount = 0, count = 0, prefix = 0;
+    int f(vector<int>& nums, int k){
+        if(k<0)return 0;
+        int l=0;
+        int r=0;
+        int cnt=0;
+        int oddCount=0;
 
-    while (right < nums.size()) {
-        // Expand the window by moving the `right` pointer
-        if (nums[right] % 2 == 1) {
-            oddCount++;
-            prefix = 0;  // Reset prefix count as we found a new odd number
-        }
-
-        // When we have exactly `k` odd numbers in the window
-        while (oddCount == k) {
-            if (nums[left] % 2 == 1) {
-                oddCount--;
+        while(r<nums.size()){
+            if(nums[r] % 2 == 1){
+                oddCount++;
             }
-            prefix++;
-            left++;
+            while(oddCount > k){
+                if(nums[l] % 2 == 1){
+                    oddCount--;
+                }
+                l++;
+            }
+            cnt+=r-l+1;
+            r++;
         }
-
-        // Add the number of valid subarrays ending at `right`
-        count += prefix;
-        right++;
+        return cnt;
     }
-
-    return count;
-
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return f(nums, k) - f(nums, k-1);
     }
 
 
